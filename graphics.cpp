4a16be55cab5d7e556395788558e5172a4e8e91f
oliverhamburger <oliver.hamburger@uvm.edu>
2020-11-18 13:03:59 -0500

    init commit

---
+++ graphics.cpp
@@ -0,0 +1,147 @@
+#include "circle.h"
+#include "rect.h"
+#include "graphics.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+GLdouble width, height;
+int wd;
+
+
+void init() {
+    width = 500;
+    height = 500;
+}
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+    // Set "clearing" or background color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black and opaque
+}
+
+/* Handler for window-repaint event. Call back when the window first appears and
+ whenever the window needs to be re-painted. */
+void display() {
+    // Tell OpenGL to use the whole window for drawing
+    glViewport(0, 0, width, height); // DO NOT CHANGE THIS LINE
+
+    // Do an orthographic parallel projection with the coordinate
+    // system set to first quadrant, limited by screen/window size
+    glMatrixMode(GL_PROJECTION); // DO NOT CHANGE THIS LINE
+    glLoadIdentity(); // DO NOT CHANGE THIS LINE
+    glOrtho(0.0, width, height, 0.0, -1.f, 1.f); // DO NOT CHANGE THIS LINE
+
+    // Clear the color buffer with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT); // DO NOT CHANGE THIS LINE
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // DO NOT CHANGE THIS LINE
+
+    /*
+     * Draw here
+     */
+
+
+    glFlush();  // Render now
+}
+
+// http://www.theasciicode.com.ar/ascii-control-characters/escape-ascii-code-27.html
+void kbd(unsigned char key, int x, int y) {
+    // escape
+    if (key == 27) {
+        glutDestroyWindow(wd);
+        exit(0);
+    }
+    if (key == 's') {
+
+    }
+    glutPostRedisplay();
+}
+
+void kbdUp(unsigned char key, int x, int y) {
+
+    if (key == 's') {
+
+    }
+    glutPostRedisplay();
+}
+
+
+void kbdS(int key, int x, int y) {
+    switch(key) {
+        case GLUT_KEY_DOWN:
+
+            break;
+        case GLUT_KEY_LEFT:
+
+            break;
+        case GLUT_KEY_RIGHT:
+
+            break;
+        case GLUT_KEY_UP:
+
+            break;
+    }
+
+    glutPostRedisplay();
+}
+
+void cursor(int x, int y) {
+
+    glutPostRedisplay();
+}
+
+// button will be GLUT_LEFT_BUTTON or GLUT_RIGHT_BUTTON
+// state will be GLUT_UP or GLUT_DOWN
+void mouse(int button, int state, int x, int y) {
+
+    glutPostRedisplay();
+}
+
+void timer(int dummy) {
+
+    glutPostRedisplay();
+    glutTimerFunc(30, timer, dummy);
+}
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+
+    init();
+
+    glutInit(&argc, argv);          // Initialize GLUT
+
+    glutInitDisplayMode(GLUT_RGBA);
+
+    glutInitWindowSize((int)width, (int)height);
+    glutInitWindowPosition(0, 0); // Position the window's initial top-left corner
+    /* create the window and store the handle to it */
+    wd = glutCreateWindow("Fun with Drawing!" /* title */ );
+
+    // Register callback handler for window re-paint event
+    glutDisplayFunc(display);
+
+    // Our own OpenGL initialization
+    initGL();
+
+    // register keyboard press event processing function
+    // works for numbers, letters, spacebar, etc.
+    glutKeyboardFunc(kbd);
+    glutKeyboardUpFunc(kbdUp);
+
+    // register special event: function keys, arrows, etc.
+    glutSpecialFunc(kbdS);
+
+    // handles mouse movement
+    glutPassiveMotionFunc(cursor);
+
+    // handles mouse click
+    glutMouseFunc(mouse);
+
+    // handles timer
+    glutTimerFunc(0, timer, 0);
+
+    // Enter the event-processing loop
+    glutMainLoop();
+    return 0;
+}
